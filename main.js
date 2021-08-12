43c594df63a4146631f93db7a717fd16f56c0983
MelodyLiu012 <57297187+MelodyLiu012@users.noreply.github.com>
2021-08-12 13:34:40 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,116 @@
+const welcomeWindow = document.getElementById("welcome");
+const closeButton = document.getElementById("close");
+
+welcomeWindow.style.opacity = 1;
+
+var closeButtonClicked = false;
+
+closeButton.onclick = () => {
+  welcomeWindow.style.opacity = 0;
+  closeButtonClicked = true;
+}
+welcomeWindow.addEventListener('transitionend', () => {
+  if (closeButtonClicked) welcomeWindow.style.zIndex = 0;
+});
+
+
+var notebookP = document.getElementById("notebookP");
+var notebook = document.getElementById("notebook");
+
+var room = document.getElementById("room");
+
+var notebookClicked = false;
+
+notebookP.onclick = () => {
+  if (!notebookClicked)
+  {
+    room.style.right = "0%";
+    notebook.style.top = "0%";
+    
+    notebookClicked = true;
+  }
+  else
+  {
+    room.style.right = "auto";
+    notebook.style.top = "-100%";
+    notebookClicked = false;
+  }
+}
+
+
+const loadingScreen = document.getElementById("loading");
+const rainAudio = document.getElementById("rain");
+
+window.addEventListener("load", event => {
+  var roomGIF = document.getElementById("roomP");
+  var isLoaded = roomGIF.complete && roomGIF.naturalHeight !== 0;
+  console.log(isLoaded);
+  loadingScreen.style.opacity = 0;
+  loadingScreen.style.zIndex = 0;
+  rainAudio.play();
+});
+
+
+const menuWindow = document.getElementById("menu");
+const menuButton = document.getElementById("threeDots");
+
+var menuButtonClicked = false;
+
+menuWindow.style.opacity = "0%";
+
+menuButton.onclick = () => {
+  if (!menuButtonClicked)
+  {
+    menuWindow.style.opacity = "100%";
+    menuButtonClicked = true;
+  }
+  else
+  {
+    menuWindow.style.opacity = "0%";
+    menuButtonClicked = false;
+  }
+}
+menuWindow.addEventListener('transitionend', () => {
+  if (menuButtonClicked) menuWindow.style.zIndex = 0;
+});
+
+
+const radioP = document.getElementById("radioP");
+var radioPlayer = document.getElementById("radioPlayer");
+
+var radioClicked = false;
+
+radioP.onclick = () => {
+  if (!radioClicked)
+  {
+    radioPlayer.play();
+    radioClicked = true;
+  }
+  else
+  {
+    radioPlayer.pause();
+    radioClicked = false;
+  }
+}
+
+const playList = 
+  [
+    "piana - Norway.mp3", 
+    "Makoto Tanaka - anoko to anoco.mp3",
+    "Choro Club - Kujira no Hirune.mp3"
+  ];
+var songIndex = 1;
+
+radioPlayer.addEventListener('ended', function(){
+  radioPlayer.pause();
+
+  if (songIndex > playList.length - 1) 
+  {
+    songIndex = 0;
+  }
+  radioPlayer.src = "assets/music/" + playList[songIndex];
+  songIndex++;
+
+  radioPlayer.load();
+  radioPlayer.play();
+});
